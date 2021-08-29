a08a7070d736d3af39777bf1444006280a0951f9
coalpha <22552895+coalpha@users.noreply.github.com>
2021-08-29 02:11:57 -0700

    workflow to ping a webserver

--- bot.js
+++ bot.js
@@ -1,3 +1,10 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("no_"));
+app.listen(port, _ => 0);
+
 const Discord = require("discord.js-light");
 
 const MSG_SWEEP_SECONDS = 3;
